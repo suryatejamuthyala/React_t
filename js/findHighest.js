464efddad286c850bbdfaa746224b9e9788490a1
Surya Muthyala <surya.muthyala@ampf.com>
2024-02-08 13:50:58 -0500

    Js assignment 1

---
+++ js/findHighest.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 20;
+const c = 30;
+
+if (a >= b && a >= c)
+  return console.log(a);
+else if (b >= c && b >= a)
+  return console.log(b)
+else return console.log(c)
+
+//Write a program to find out what is biggest of 3 numbers
---
+++ js/operators.js
@@ -0,0 +1,10 @@
+let y = 5;
+let x = y + 2;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x / y);
+console.log(x * y);
+console.log(x % y);
+
+//Perform an program to perform all the operations in js
