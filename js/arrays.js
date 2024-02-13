11fe7ee17b8bfed5a2cf510db74951ef89ff9be1
Surya Muthyala <surya.muthyala@ampf.com>
2024-02-13 15:05:03 -0500

    duplicates and oddeven

--- js/arrays.js
+++ js/arrays.js
@@ -32,3 +32,21 @@ for (let i = 0; i < numbers.length; i++) {
 console.log(numbers)
 console.log(numbers2)
 console.log(addition)
+
+
+let oddArray = []
+let evenArray= []
+  for (i = 0; i < 10; i++){
+    if (i % 2 === 0) {
+      evenArray.push(i)
+    }
+    else oddArray.push(i);
+}
+console.log("odd Numbers:", oddArray)
+console.log("even Numbers:", evenArray)
+
+let a = [1, 8, 7, 9, 1]
+
+const hasDuplicates = (arr) => arr.length !== new Set(arr).size;
+
+console.log(hasDuplicates(a))
