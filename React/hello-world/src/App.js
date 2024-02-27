4a25acad467a35d0831eb501e10fd2fa03f6b010
dileep <deelipkumar261997@gmail.com>
2024-02-27 21:19:19 +0530

    Added Hello World in react

---
+++ React/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HelloComponent = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a basic React component.</p>
+    </div>
+  );
+};
+
+export default HelloComponent;
