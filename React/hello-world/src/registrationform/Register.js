fce2011f1d97f2a131e4629f519a3b29a7ad1dd0
dileep <deelipkumar261997@gmail.com>
2024-03-02 17:29:33 +0530

    Added change in register js

--- React/hello-world/src/registrationform/Register.js
+++ React/hello-world/src/registrationform/Register.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './register.css';
 
 const Register = (props) => {
     const [email, setEmail] = useState('')
@@ -9,18 +10,22 @@ const Register = (props) => {
         e.preventDefault();
     }
 
-    return (<>
-            <form onSubmit={handleSubmit}>
+    return (<><div className='form-container'>
+           <center><form onSubmit={handleSubmit} style={{position:'center',top:'50%'}} >
                 <label>Full name</label>
                 <input value={name} onChange={(e) => setName(e.target.value)}/>
+                <br></br>
                 <label htmlFor="email">email</label>
                 <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <br></br>
                 <label htmlFor="password">password</label>
                 <input type='password' id='password' name='password' value={password}
+
                        onChange={(e) => setPassword(e.target.value)}/>
+                       <br></br>
                 <button type='submit'>Submit</button>
             </form>
-            <button onClick={() => props.onFormSwitch('login')}>Go Back</button>
+            <button onClick={() => props.onFormSwitch('login')}>Go Back</button></center> </div>
         </>
 
     );
